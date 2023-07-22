df6ff7403080fac36e36b9252200d04f8c122a6f
Kunal2703 <kunalsingh2703@gmail.com>
2023-07-22 22:46:23 +0530

    Adds form validation for birthYear input field

--- static/index.js
+++ static/index.js
@@ -1,6 +1,16 @@
 function calculateAge() {
     const form = document.getElementById("ageForm");
+    const birthYearInput = form.elements["birthYear"];
     const formData = new FormData(form);
+
+    const yearPattern = /^[0-9]{4}$/;
+    const errorMessage = document.getElementById("error-message");
+    if (!yearPattern.test(birthYearInput.value)) {
+        errorMessage.removeAttribute("hidden");
+        return;
+    } else {
+        errorMessage.setAttribute("hidden", true);
+    }
 
     fetch('/calculate', {
         method: 'POST',
